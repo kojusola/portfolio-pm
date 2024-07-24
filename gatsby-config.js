dd1197d22e095956d2dc8fdaf49e44ea8dce6070
kojusola <adeola5678@gmail.com>
2024-07-24 01:36:17 +0100

    feat: update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Adeola Pm Portfolio`,
+    description: `Adeola Product management portfolio`,
+    author: `@adeola`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
@@ -28,7 +28,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/red-eye-image.png`, // This path is relative to the root of the site.
       },
     },
     {
